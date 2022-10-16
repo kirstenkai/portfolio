5f09d24298aef40a4665c06677eb213bd5891f7d
KC Kai <kirstenckai@gmail.com>
2022-10-16 13:08:51 -0400

    updates config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Kirsten Kai',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Kirsten Kai is a software developer specializing in building exceptional digital experiences for the web.',
+    siteUrl: 'https://kirstenkai.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +20,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'BrittanyChiang',
-        short_name: 'BrittanyChiang',
+        name: 'KirstenKai',
+        short_name: 'KirstenKai',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
